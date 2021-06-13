5d8975236fa4fa1aae035c1ead97b2798ad27595
cheikhkane <serigneckane@gmail.com>
2021-06-13 00:56:23 +0000

    delete

--- AskaanbiibanqueG1-omar/delete_agent.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'database.php';
-$idAgent = $_GET['id'];
-$sql = 'DELETE FROM agent WHERE idAgent=:idAgent';
-$statement = $connection->prepare($sql);
-if ($statement->execute([':idAgent' => $idAgent])) {
-  header("Location: /");
-}
-?>
